1812b17bc7c943107a4311186e9e2aa1645ce878
hungbui7690 <hungbui7690@gmail.com>
2023-06-12 09:55:54 +0700

    Handling Events P1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 /*
-  Conditional Rendering P4
+  Handling Events P1
 
 
 */
--- client/src/components/ListGroup.tsx
+++ client/src/components/ListGroup.tsx
@@ -2,18 +2,18 @@ import { Fragment } from 'react'
 
 function ListGroup() {
   let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris']
-  items = []
 
   return (
     <Fragment>
       <h1>List</h1>
-
-      {/* use && >> we don't need to use null  */}
       {items.length === 0 && <p>No Item Found</p>}
-
       <ul className='list-group'>
-        {items.map((item) => (
-          <li className='list-group-item' key={item}>
+        {items.map((item, index) => (
+          <li
+            className='list-group-item'
+            key={item}
+            onClick={() => console.log({ item, index })} // *** here
+          >
             {item}
           </li>
         ))}
